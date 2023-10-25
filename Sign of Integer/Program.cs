f2aa3ca0314b3e59dd4e200e64fa3e5fefbfb575
boriskisiov <boriskisiov@gmail.com>
2023-10-25 15:23:22 +0300

    Add project files.

---
+++ Sign of Integer/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+static void PrintnumberSign (int number)
+{
+    if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero!");
+    }
+    else if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive!");
+    }
+    else
+    {
+        Console.WriteLine($"The number{number} is negative!");
+    }
+}
+int number = int.Parse(Console.ReadLine());
+PrintnumberSign(number);
+
+int number2 = int.Parse(Console.ReadLine());
+PrintnumberSign(number2);
